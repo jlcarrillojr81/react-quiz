d18e3cb780d177b6c187b484c80d2577e75f9222
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-07-12 05:10:37 -1000

    failed

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,34 @@ import LoggedOut from './components/LoggedOut'
 import LoggedIn from './components/LoggedIn'
 
 
+const [loggedIn, setLoggedIn] = useState(true);
+
+
 function App() {
+  if (!LoggedIn)
+    
+  return (
+    <>
+      {/* <LoggedIn /> */}
+      <LoggedOut />
+    </>
+  )
+  
 
   // Hook up the functionality for the LoggedIn component to show when the loggedIn state is true
   // and the LoggedOut component to show when loggedIn state is false.
 
   // See instruction ins toggleLogIn function
 
-  const [loggedIn, setLoggedIn] = useState(true)
-
   const toggleLogIn = () => {
+    setLoggedIn(!loggedIn);
     // should change the state of loggedIn to whatever it's opposite is. 
     // Remember to pass this prop down to the button component, and hook it up there.
   }
 
-  if(loggedIn) {
-    return <LoggedIn />
-  }
+ 
 
-  return <LoggedOut />
+
 
 
 }
